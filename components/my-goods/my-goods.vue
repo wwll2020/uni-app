70ac9871d100c18349389fce8bfbaf71b7279947
wanglin <wanglin199803@foxmail.com>
2021-06-08 18:09:35 +0800

    完成了购物车的开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -1,13 +1,18 @@
 <template>
   <view class="goods-item">
     <!-- 左侧的盒子 -->
-    <view class="goods-item-left"><image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image></view>
+    <view class="goods-item-left">
+      <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
     <!-- 右侧的盒子 -->
     <view class="goods-item-right">
       <!-- 商品的名字 -->
       <view class="goods-name">{{ goods.goods_name }}</view>
       <view class="goods-info-box">
-        <view class="goods-price">￥{{ goods.goods_price | toFixed}}</view>
+        <view class="goods-price">￥{{ goods.goods_price | toFixed }}</view>
+        <!-- 商品数量 -->
+        <uni-number-box :min="1" :value="goods.goods_count" v-if="showNum" @change="numChangeHandler"></uni-number-box>
       </view>
     </view>
   </view>
@@ -18,6 +23,16 @@ export default {
     goods: {
       type: Object,
       defaul: {}
+    },
+    // 是否展示图片左侧的 radio
+    showRadio: {
+      type: Boolean, // 如果外界没有指定 show-radio 属性的值，则默认不展示 radio 组件
+      default: false
+    },
+    // 是否展示价格右侧的 NumberBox 组件
+    showNum: {
+      type: Boolean,
+      default: false
     }
   },
   name: 'my-goods',
@@ -27,9 +42,29 @@ export default {
       defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
     };
   },
-  filters:{
-    toFixed(num){
-      return Number(num).toFixed(2)
+  filters: {
+    toFixed(num) {
+      return Number(num).toFixed(2);
+    }
+  },
+  methods: {
+    radioClickHandler() {
+      // 通过 this.$emit() 触发外界通过 @ 绑定的 radio-change 事件，
+      // 同时把商品的 Id 和 勾选状态 作为参数传递给 radio-change 事件处理函数
+      this.$emit('radio-change', {
+        // 商品的 Id
+        goods_id: this.goods.goods_id,
+        // 商品最新的勾选状态
+        goods_state: !this.goods.goods_state
+      });
+    },
+    // NumberBox 组件的 change 事件处理函数
+    numChangeHandler(val) {
+      // 通过 this.$emit() 触发外界通过 @ 绑定的 num-change 事件
+      this.$emit('num-change', {
+        goods_id: this.goods.goods_id, // 商品的 Id
+        goods_count: +val // 商品的最新数量
+      });
     }
   }
 };
@@ -37,12 +72,19 @@ export default {
 
 <style lang="scss">
 .goods-item {
+  // 让 goods-item 项占满整个屏幕的宽度 
+  width: 750rpx; 
+  // 设置盒模型为 border-box 
+  box-sizing: border-box;
   display: flex;
   padding: 10px 5px;
   border-bottom: 1px solid #f0f0f0;
 
   .goods-item-left {
     margin-right: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
     .goods-pic {
       width: 100px;
@@ -53,6 +95,7 @@ export default {
 
   .goods-item-right {
     display: flex;
+    flex: 1;
     flex-direction: column;
     justify-content: space-between;
 
@@ -61,6 +104,9 @@ export default {
     }
 
     .goods-info-box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
       .goods-price {
         color: #c00000;
         font-size: 16px;
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App'
+// 1. 导入 store 的实例对象 
+import store from './store/store.js'
 
 import {$http} from '@escook/request-miniprogram'
 uni.$http = $http
@@ -16,6 +18,8 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+  ...App,
+  // 2. 将 store 挂载到 Vue 实例上
+   store,
 })
 app.$mount()
